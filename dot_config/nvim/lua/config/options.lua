79731ce8c4c9b506e88bd4266edae7a76ce57725
Georges <georges.plancke@gmail.com>
2025-06-09 09:42:37 +0200

    Remove .config/nvim/LICENSE Remove .config/nvim/README.md Remove .config/nvim/.gitignore Remove .config/nvim/.neoconf.json Remove .config/nvim/init.lua Remove .config/nvim/lazy-lock.json Remove .config/nvim/lazyvim.json Remove .config/nvim/lua/config/autocmds.lua Remove .config/nvim/lua/config/keymaps.lua Remove .config/nvim/lua/config/lazy.lua Remove .config/nvim/lua/config/options.lua Remove .config/nvim/lua/plugins/added.lua Remove .config/nvim/lua/plugins/changed.lua Remove .config/nvim/lua/plugins/example.lua Remove .config/nvim/stylua.toml

--- dot_config/nvim/lua/config/options.lua
+++
@@ -1,7 +0,0 @@
--- Options are automatically loaded before lazy.nvim startup
--- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
--- Add any additional options here
---
--- Do or do not use blink/cmp for ai completion
-vim.g.ai_cmp = true
-vim.g.root_spec = { { ".git", ".hg", ".svn" } }
