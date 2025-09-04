2f0b92e56c7bc4368c0103763c7d62f7b5d76796
Georges <georges.plancke@gmail.com>
2025-09-04 14:18:30 +0200

    Update .config/nvim/lazy-lock.json Update .config/nvim/lua/plugins/changed.lua

--- dot_config/nvim/lua/plugins/changed.lua
+++ dot_config/nvim/lua/plugins/changed.lua
@@ -89,6 +89,12 @@ return {
   },
   {
     "akinsho/bufferline.nvim",
+    init = function()
+      local bufline = require("catppuccin.groups.integrations.bufferline")
+      function bufline.get()
+        return bufline.get_theme()
+      end
+    end,
     opts = function(_, opts)
       -- Try to always show bufferline
       opts.options = opts.options or {}
