4cd441198f0a71b946cbde803a99951cf44c35d1
Georges <georges.plancke@gmail.com>
2025-11-15 17:11:52 +0100

    Update .config/nix/flake.lock Update .config/nix/flake.nix

--- dot_config/nix/flake.nix
+++ dot_config/nix/flake.nix
@@ -15,44 +15,44 @@
       homies = pkgs.buildEnv {
         name = "homies";
         paths = [
-          pkgs.cacert
-          pkgs.nix-diff
-          pkgs.gcc
-          pkgs.bash
-          pkgs.zsh
-					pkgs.fzf
-          pkgs.tree
-          pkgs.age
-          pkgs.unzip
-          pkgs.bat
-          pkgs.jq
-          pkgs.jc
-          pkgs.sq
-          pkgs.zoxide
-          pkgs.ripgrep
-          pkgs.silver-searcher
-					pkgs.diff-so-fancy
-					pkgs.starship
-
-          pkgs.mkcert
-          pkgs.socat
-          pkgs.nmap
-
-					pkgs.direnv
-
-					pkgs.git
-					pkgs.gh
-					pkgs.hub
-					pkgs.fossil
-
-					pkgs.ttyd
-					pkgs.tmux
-					pkgs.neovim
-
-          pkgs.htop
-          pkgs.gtop
-					pkgs.lazydocker
-					pkgs.lazygit
+					pkgs.cacert
+					pkgs.nix-diff
+					# pkgs.gcc
+					# pkgs.bash
+					# pkgs.zsh
+					# pkgs.fzf
+					# pkgs.tree
+					# pkgs.age
+					# pkgs.unzip
+					# pkgs.bat
+					# pkgs.jq
+					# pkgs.jc
+					# pkgs.sq
+					# pkgs.zoxide
+					# pkgs.ripgrep
+					# pkgs.silver-searcher
+					# pkgs.diff-so-fancy
+					# pkgs.starship
+					#
+					# pkgs.mkcert
+					# pkgs.socat
+					# pkgs.nmap
+					#
+					# pkgs.direnv
+					#
+					# pkgs.git
+					# pkgs.gh
+					# pkgs.hub
+					# pkgs.fossil
+					#
+					# pkgs.ttyd
+					# pkgs.tmux
+					# pkgs.neovim
+					#
+					# pkgs.htop
+					# pkgs.gtop
+					# pkgs.lazydocker
+					# pkgs.lazygit
         ];
       };
 
