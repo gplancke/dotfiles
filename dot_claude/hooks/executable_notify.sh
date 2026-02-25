240d8ede95d2d368e3fb0da2e68d76b4b2301b19
Georges <georges.plancke@gmail.com>
2026-02-25 18:10:39 +0100

    adding pushover

--- dot_claude/hooks/executable_notify.sh
+++ dot_claude/hooks/executable_notify.sh
@@ -9,3 +9,13 @@ if command -v terminal-notifier &>/dev/null; then
 else
   osascript -e "display notification \"$message\" with title \"$title\" sound name \"Glass\""
 fi
+
+# Pushover push notification
+if [[ -n "$PUSHOVER_KEY" && -n "$PUSHOVER_CLAUDE_TOKEN" ]]; then
+  curl -s -o /dev/null \
+    --form-string "token=$PUSHOVER_CLAUDE_TOKEN" \
+    --form-string "user=$PUSHOVER_KEY" \
+    --form-string "title=$title" \
+    --form-string "message=$message" \
+    https://api.pushover.net/1/messages.json &
+fi
